ce06a5a4a41091e12e1ad3af13dc876abc62c73d
lue <julian.luetgert@zigmos.de>
2023-07-23 14:07:09 +0200

    Allow to add a link to the first attached file
    $F and $f will be replaced by absolute and relative paths to the
    attached file, respectively.
    To work, one needs [plenary.nvim](https://github.com/nvim-lua/plenary.nvim)
    
    Note, that currently, relative paths might behave somewhat unintuitive
    See https://github.com/nvim-lua/plenary.nvim/issues/411

--- lua/telescope/_extensions/papis/actions.lua
+++ lua/telescope/_extensions/papis/actions.lua
@@ -6,17 +6,28 @@
 
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
+local Path = require("plenary.path")
 
 local utils = require("papis.utils")
 
 local M = {}
 
 ---This function inserts a formatted reference string at the cursor
----@param format_string string @The string to be inserted
+---@param format_string string @The string to be inserted. 
+---       $F will be replaced with the absolute location of the first file
+---       attached to the entry, $f with the relative path.
 ---@return function
 M.ref_insert = function(format_string)
   return function(prompt_bufnr)
     local entry = string.format(format_string, action_state.get_selected_entry().id.ref)
+    if action_state.get_selected_entry().id.files == nil then
+        entry = entry:gsub("$F", "")
+        entry = entry:gsub("$f", "")
+    else
+        local file_path = Path:new{action_state.get_selected_entry().id.files[1]}
+        entry = entry:gsub("$F", file_path:normalize())
+        entry = entry:gsub("$f", file_path:make_relative())
+    end
     actions.close(prompt_bufnr)
     vim.api.nvim_put({ entry }, "", false, true)
   end
